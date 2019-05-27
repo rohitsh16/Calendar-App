d7d41a2d982de020dee6a56d2a05fbbc58ca9b8f
rohitsh16 <rohitshukla7970@gmail.com>
2019-05-27 18:21:55 +0530

    first commit

---
+++ cal/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.urls import reverse
+
+#Create Your Models Here
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    def _str_(self):
+        return str(self.pk)
+
+    @property
+    def get_html_url(self):
+        url = reverse('cal:event_edit', args=(self.id,))
+        return f'<a href="{url}"> {self.title} </a>'
+        
+    
---
+++ cal/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from . import views
+
+
+app_name = 'cal'
+urlpatterns = [
+    #url('index', views.index, name='index'),
+    url('calendar', views.CalendarView.as_view(), name='calendar'),
+    #url('', views.CalendarView.as_view(), name='calendar'),
+    url('event/new/', views.event, name='event_new'),
+	url('event/edit/(?P<event_id>\d+)/', views.event, name='event_edit'),
+    ]
